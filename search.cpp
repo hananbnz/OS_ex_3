7b4f3e314b7b282f9fbde4d08bcac4a47a3f92a0
reuveny <reuven.yimmer@mail.huji.ac.il>
2017-05-07 16:49:02 +0300

    project start 3

--- search.cpp
+++ search.cpp
@@ -2,3 +2,12 @@
 // Created by reuveny on 5/7/17.
 //
 
+#include <cstdio>
+
+int main(void)
+{
+	printf("search running....\n");
+    printf("search for 'os' in os2015/exercise blabla myFolder\n");
+    printf("found 'osTargil' 'sos' 'sos'\n");
+    printf("search finished...\n");
+}
