73d71c3403fd9d03c6740e98010fd452c8955f53
hananbnz <hananbnz@gmail.com>
2017-05-10 21:39:04 +0300

    day 1 start MapReduceFramework

--- MapReduceFramework.cpp
+++ MapReduceFramework.cpp
@@ -3,3 +3,32 @@
 //
 
 #include "MapReduceFramework.h"
+#include <pthread.h>
+
+#define CHUNK_SIZE 10
+
+// TODO implement RunMapReduceFramework, Emit2, Emit3
+class MapReduceFramework
+{
+
+};
+
+/**
+ * 1. Create ExecMap threads (pthreads) - each one of them will exec chunk of
+ * pairs in the map func
+ */
+
+
+OUT_ITEMS_VEC RunMapReduceFramework(MapReduceBase& mapReduce, IN_ITEMS_VEC& itemsVec,
+                                    int multiThreadLevel, bool autoDeleteV2K2)
+{
+    for (int i = 0; i <multiThreadLevel ; ++i)
+    {
+        // TODO creation of pthread
+        pthread_t* cur_thread = pthread_create();
+
+    }
+    // create the map size multiThreadLevel each with key - thread ID, val -
+    // the thread ID container
+}
+
